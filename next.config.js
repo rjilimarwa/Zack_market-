44d7428cb8cc88379a48d230fbdd4684c36e01eb
rjilimarwadjerba@gmail.com <admin@exemple.com>
2021-02-01 15:15:31 +0000

    add page login

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 module.exports= {
  env:{
  "BASE_URL":"http://localhost:3000",
- "MONGODB_URL":"mongodb+srv://devat:123456789azerty@cluster0.hwkif.mongodb.net/nextjs_ecommerce?retryWrites=true&w=majority"
+ "MONGODB_URL":"mongodb+srv://devat:123456789azerty@cluster0.hwkif.mongodb.net/nextjs_ecommerce?retryWrites=true&w=majority",
+  "ACCESS_TOKEN_SECRET": "$mtyBEV*g+/76Qm@Z8(wXA]{axeESH+g(:,P~nHd'6(#S^h@",
+  "REFRESH_TOKEN_SECRET": "%C!G[H;vR6P^U(n2_p2_{@}B>nhR@9Ve_77{+/pm''^-M<tu=MrLFsWB~zj-~M*cg$Q)^~cx2y,8",
 }
 
 }
---
+++ pages/api/auth/accessToken.js
@@ -0,0 +1,13 @@
+import connectDB from '../../../utils/connectDB';
+import Users from '../../../models/userModel';
+import bcrypt from 'bcrypt';
+import {createAccessToken,createRefreshToken} from '../../../utils/generateToken'
+
+connectDB()
+export default async(req,res)=>{
+    try{
+
+    }catch{
+
+    }
+}
