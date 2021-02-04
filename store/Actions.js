993b791ec14c77e792812924e2f2b854c88c9a8a
rjilimarwadjerba@gmail.com <admin@exemple.com>
2021-02-04 16:25:46 +0000

    add cart page

--- store/Actions.js
+++ store/Actions.js
@@ -1,8 +1,34 @@
 export const ACTIONS = {
     NOTIFY: 'NOTIFY',
-    AUTH: 'AUTH'
+    AUTH: 'AUTH',
+    ADD_CART:'ADD_CART'
 
 }
 
+export const addToCart=(product,cart)=>{
+    if(product.inStock ===0)
+        return ({type:'NOTIFY',payload:{error:'This product is out of stock'}})
+    const check=cart.every(item =>{
+         return item._id !==product._id
+        })
+    if(!check) return({type:'Notify',payload:{error:'this product has been add to cart'}})
+    return ({type:'Notify',payload:[...cart,{...product,quantity:1}]})
+}
 
+export const decrease = (data, id) => {
+    const newData = [...data]
+    newData.forEach(item => {
+        if(item._id === id) item.quantity -= 1
+})
 
+    return ({ type: 'ADD_CART', payload: newData })
+}
+
+export const increase = (data, id) => {
+    const newData = [...data]
+    newData.forEach(item => {
+        if(item._id === id) item.quantity += 1
+})
+
+    return ({ type: 'ADD_CART', payload: newData })
+}
